168fc1b6e07c87b4023f9b02ef9a1c2b3b20eec6
deividastamosaitis <tamosaitis.deividas@gmail.com>
2024-02-07 15:44:27 +0200

    pasiulyta edit

--- utils/constants.js
+++ utils/constants.js
@@ -1,20 +1,20 @@
 export const JOB_STATUS = {
-  Expo: 'Ekspozicija',
-  Montavimas: 'Montavimas',
-  MontavimasSkubu: 'Montavimas-SKUBU',
-  Pasiulyta: 'Pasiulyta',
-  Baigta: 'Baigta',
+  Expo: "Ekspozicija",
+  Montavimas: "Montavimas",
+  MontavimasSkubu: "Montavimas-SKUBU",
+  Pasiulyta: "Pasiulyta",
+  Baigta: "Baigta",
 };
 
 export const JOB_TYPE = {
-  FULL_TIME: 'full-time',
-  PART_TIME: 'part-time',
-  INTERNSHIP: 'internship',
+  FULL_TIME: "full-time",
+  PART_TIME: "part-time",
+  INTERNSHIP: "internship",
 };
 
 export const JOB_SORT_BY = {
-  NEWEST_FIRST: 'newest',
-  OLDEST_FIRST: 'oldest',
-  ASCENDING: 'a-z',
-  DESCENDING: 'z-a',
+  NEWEST_FIRST: "newest",
+  OLDEST_FIRST: "oldest",
+  ASCENDING: "a-z",
+  DESCENDING: "z-a",
 };

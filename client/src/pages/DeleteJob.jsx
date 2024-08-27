8a69657d8d3d55e2f6a2f5ad9e5d7135a5d70f7d
deividas <tamosaitis.deividas@gmail.com>
2024-08-27 20:05:32 +0300

    sulietuvinta

--- client/src/pages/DeleteJob.jsx
+++ client/src/pages/DeleteJob.jsx
@@ -1,13 +1,13 @@
-import { toast } from 'react-toastify';
-import customFetch from '../utils/customFetch';
-import { redirect } from 'react-router-dom';
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
+import { redirect } from "react-router-dom";
 
 export const action = async ({ params }) => {
   try {
     await customFetch.delete(`/jobs/${params.id}`);
-    toast.success('Job deleted successfully');
+    toast.success("Objektas ištrintas sėkmingai");
   } catch (error) {
     toast.error(error?.response?.data?.msg);
   }
-  return redirect('/dashboard/all-jobs');
+  return redirect("/dashboard/all-jobs");
 };
